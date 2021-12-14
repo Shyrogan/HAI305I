8501a21b3ca23097617e9ada395540f631d7ad3c
Sébastien VIAL <sebastien.vial@etu.umontpellier.fr>
2021-12-14 19:27:40 +0100

    Amélioration de la recherche et SQL

--- boutique.php
+++ boutique.php
@@ -7,7 +7,7 @@ $marq = isset($_GET["marque"]) ?  $_GET["marque"] : "";
 echo '<div class="page-container container">';
   echo '<p class="p-4"><label class="pr-4">Recherche : </label><input type="text" value="" id="nom" name="nom" /></p>';
 while ($Lproduits = $produits->fetch()) {
-  echo '<div class="row" id="'.$Lproduits['nom'].'">';
+  echo '<div class="row" id="'.$Lproduits['nom'].' '.$Lproduits['marque'].' '.$Lproduits['prix'].' '.$Lproduits['descriptif'].'">';
     echo '<div class="col">';
       echo '<img src="'.$Lproduits['photo'].'" class="imgproduits">';
     echo '</div>';
